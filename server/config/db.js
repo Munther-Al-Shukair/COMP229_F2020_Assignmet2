390169f6bdb2ab76e71825043d67ab0ab360f651
Munther Al Shukair <monthershqair@gmail.com>
2020-10-27 16:07:07 -0400

    update the URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/contact_list"
+    //"URI": "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://Munther:3SlaSB01p2OmjLHH@mongodbserver.0u5lj.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
